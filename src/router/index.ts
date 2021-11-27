3738527c4626e2be57ee9a0f7980ae2db3e82eca
haibolian <957377783@qq.com>
2021-11-27 15:10:24 +0800

    history模式

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory, RouteRecordRaw } from 'vue-router'
+import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
 import Home from '../views/Home.vue'
 const routes: Array<RouteRecordRaw> = [
   {
@@ -9,8 +9,12 @@ const routes: Array<RouteRecordRaw> = [
 ]
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(),
   routes
+})
+
+router.beforeEach((to,from)=>{
+  
 })
 
 export default router
