11a6fb0408477f4b1128adc1f33ebcdb1a7ae7fb
lianhaibo <957377783@qq.com>
2022-04-16 10:45:14 +0800

    deploy

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,7 @@ const path = require('path')
 
 module.exports = {
   publicPath: '/chinese-colors/',
+  homepage: "https://haibolian.github.io/chinese-colors",
   chainWebpack(config){
     config.plugin('html').tap(args => {
       args[0].title = '中国色 - 中国传统颜色'
